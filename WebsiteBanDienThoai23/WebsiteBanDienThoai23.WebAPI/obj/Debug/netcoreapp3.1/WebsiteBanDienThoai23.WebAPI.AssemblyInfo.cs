14947e897c1a60a96a761a50c8574413f596a282
Lê Minh Tính <tinhcv181@gmail.com>
2024-05-16 03:39:59 +0700

    u

--- WebsiteBanDienThoai23/WebsiteBanDienThoai23.WebAPI/obj/Debug/netcoreapp3.1/WebsiteBanDienThoai23.WebAPI.AssemblyInfo.cs
+++ WebsiteBanDienThoai23/WebsiteBanDienThoai23.WebAPI/obj/Debug/netcoreapp3.1/WebsiteBanDienThoai23.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsiteBanDienThoai23.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca18043c89640570dac00a732f556a37cb2e9c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075663a0efc2697cfcc66e03d0a313b40381d9e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsiteBanDienThoai23.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsiteBanDienThoai23.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

a510098969bf143c884dd2318a4eb2e4dae5ac19
Lê Minh Tính <tinhcv181@gmail.com>
2024-05-18 13:37:31 +0700

    done home, dienthoai, maycu

--- WebsiteBanDienThoai23/WebsiteBanDienThoai23.Web/obj/Debug/netcoreapp3.1/WebsiteBanDienThoai23.Web.AssemblyInfo.cs
+++ WebsiteBanDienThoai23/WebsiteBanDienThoai23.Web/obj/Debug/netcoreapp3.1/WebsiteBanDienThoai23.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsiteBanDienThoai23.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9690214b3682d59cb3d121d7958550bfff476f66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsiteBanDienThoai23.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsiteBanDienThoai23.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

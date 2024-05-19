295ce5d0321c2c89fa8f6a073efadd99a377cc10
Nguyễn Thái Quốc <quochucau2016@gmail.com>
2024-05-19 18:39:19 +0700

    update

--- WebsiteBanDienThoai23/WebsiteBanDienThoai23.Common/obj/Debug/netcoreapp3.1/WebsiteBanDienThoai23.Common.AssemblyInfo.cs
+++ WebsiteBanDienThoai23/WebsiteBanDienThoai23.Common/obj/Debug/netcoreapp3.1/WebsiteBanDienThoai23.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsiteBanDienThoai23.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7aa5c36a32c06fbd708e7488903e6557f613593")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272de41d3315b0e41bdff17c034a9dadb42ce4d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsiteBanDienThoai23.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsiteBanDienThoai23.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
